1f3e35c6ba2da67218a21e740de540c9f60fa4b2
Jackie <JGtheophila19@gmail.com>
2018-05-21 11:23:11 -0400

    Create symmetric.js

---
+++ symmetric.js
@@ -0,0 +1,10 @@
+// create a function
+// takes in 2 or more arrays
+/* return an array of symmetric difference - of two sets is the set of elements which are in either of the two sets, but not in both */
+
+
+function sym(args) {
+  return args;
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
